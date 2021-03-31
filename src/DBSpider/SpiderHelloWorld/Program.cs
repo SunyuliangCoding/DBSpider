a1e598b1fdc98a41e357d5d09f2f195ad53a23ea
sunyuliang <sunyuliang@beisen.com>
2021-03-31 22:21:37 +0800

    添加gitignore

---
+++ src/DBSpider/SpiderHelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpiderHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
